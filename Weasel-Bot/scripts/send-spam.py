368b22610a707c1bb6905810132d42fafcef2d3d
Dan Garant <dgarant@cs.umass.edu>
2014-12-15 10:42:53 -0500

    Re-organizing script directory

---
+++ Weasel-Bot/scripts/send-spam.py
@@ -0,0 +1,31 @@
+def sendMail(subject, text):
+    import os
+    import smtplib
+    import mimetypes
+    import email
+    gmailUser = 'weaselkeene@gmail.com'
+    gmailPassword = 'weaselbotnet!'
+    recipient = '<enter recipient name>'
+    msg = email.MIMEMultipart.MIMEMultipart()
+    msg['From'] = gmailUser
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.attach(email.MIMEText.MIMEText(text))
+    mailServer = smtplib.SMTP('smtp.gmail.com', 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(gmailUser, gmailPassword)
+    mailServer.sendmail(gmailUser, recipient, msg.as_string())
+    mailServer.close()
+    print('Sent email to %s' % recipient)
+
+content = (
+            "Dear Spam Recipient,\n\n"
+            "I hope you are having a lovely day.\n\n"
+            "Sincerely,\n"
+            "Weasel\n"
+          )
+
+sendMail('Some Spam from Weasel', content)
+
